65e9e510b9577e4f316ddaf10f047a92347f302c
Mykola Chaikovskyi <nickchaywork@gmail.com>
2020-11-02 19:08:03 +0200

    Добавлен код инициализации для менеджера кэша

--- src/main/java/live/nickp0is0n/serverlogatsamp/ui/CacheManagerController.java
+++ src/main/java/live/nickp0is0n/serverlogatsamp/ui/CacheManagerController.java
@@ -1,13 +1,29 @@
 package live.nickp0is0n.serverlogatsamp.ui;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CacheManagerController {
 
     @FXML
     private ListView<String> cacheListView;
+
+    @FXML
+    void initialize() {
+        File cacheDirectory = new File("downloadedLogs");
+        ArrayList<File> cacheFiles = new ArrayList<>(Arrays.asList(cacheDirectory.listFiles()));
+        List<String> cacheFileNames = new ArrayList<String>();
+        cacheFiles.forEach((it) -> cacheFileNames.add(FilenameUtils.removeExtension(it.getName())));
+        cacheListView.setItems(FXCollections.observableArrayList(cacheFileNames));
+    }
 
     @FXML
     void onChangeNameButtonClick(ActionEvent event) {
