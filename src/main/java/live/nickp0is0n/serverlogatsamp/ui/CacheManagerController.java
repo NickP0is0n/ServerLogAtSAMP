36b6fe09959f4f9ea2e6796aee6601e09dc4b68c
Mykola Chaikovskyi <nickchaywork@gmail.com>
2020-11-02 18:54:24 +0200

    Добавлен базовый код для менеджера кэша

---
+++ src/main/java/live/nickp0is0n/serverlogatsamp/ui/CacheManagerController.java
@@ -0,0 +1,27 @@
+package live.nickp0is0n.serverlogatsamp.ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class CacheManagerController {
+
+    @FXML
+    private ListView<String> cacheListView;
+
+    @FXML
+    void onChangeNameButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLoadButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRemoveButtonClick(ActionEvent event) {
+
+    }
+
+}
